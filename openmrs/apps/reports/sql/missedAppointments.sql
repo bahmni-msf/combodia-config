4bd9434c5086886783d40b4c88488159c8fd6e85
Vineela Vineela <vineela.vini421.vv@gmail.com>
2018-12-14 15:23:41 +0530

    Vineela|Add sql file for missedAppointments

---
+++ openmrs/apps/reports/sql/missedAppointments.sql
@@ -0,0 +1,20 @@
+SELECT patient_identifier.identifier AS ID, concat(person_name.given_name,' ',person_name.family_name) AS Name, person_attribute.value AS PhoneNumber,appointment_service.name AS ServiceType
+FROM patient_appointment
+
+JOIN
+patient_identifier ON
+patient_identifier.patient_id=patient_appointment.patient_id
+JOIN
+person_name ON
+person_name.person_id=patient_appointment.patient_id
+JOIN
+appointment_service ON
+appointment_service.appointment_service_id=patient_appointment.appointment_service_id
+JOIN
+person_attribute ON
+person_attribute.person_id=patient_appointment.patient_id
+
+WHERE DATE(patient_appointment.start_date_time) BETWEEN DATE('#startDate#') AND DATE('#endDate#')
+AND patient_appointment.status='Missed'
+AND person_attribute.person_attribute_type_id=13
+AND appointment_service.appointment_service_id=patient_appointment.appointment_service_id
