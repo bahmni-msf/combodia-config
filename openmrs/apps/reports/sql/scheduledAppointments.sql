054976547bd1966f91fa23d2f58468c136bf426a
vishnu <dvishnu@thoughtworks.com>
2019-01-04 12:36:52 +0530

    Vishnu|Adding the previous Renaming format of the scheduled report

--- openmrs/apps/reports/sql/scheduledAppointments.sql
+++ openmrs/apps/reports/sql/scheduledAppointments.sql
@@ -1,12 +1,12 @@
-SELECT
-patient_identifier.identifier AS ID,
-concat(person_name.given_name,' ',person_name.family_name) AS Name,
-person_attribute.value AS PhoneNumber,appointment_service.name AS ServiceType,
-DATE(patient_appointment.start_date_time) AS DateOfAppointment,
+SELECT 
+patient_identifier.identifier AS "Patient ID",
+concat(person_name.given_name,' ',person_name.family_name) AS "Patient Name",
+person_attribute.value AS "Phone Number",appointment_service.name AS "Service Type",
+DATE(patient_appointment.start_date_time) AS "Date Of Scheduled Appointment",
 (case when pat.patient_appointment_id < patient_appointment.patient_appointment_id  then case when pat.patient_appointment_id < patient_appointment.patient_appointment_id
-then pat.status else null end else null end) as "PreviousAppointmentStatus",
+then pat.status else null end else null end) as "Previous Appointment Status",
 (case when pat.patient_appointment_id < patient_appointment.patient_appointment_id  then case when pat.patient_appointment_id < patient_appointment.patient_appointment_id
-then date(pat.start_date_time) else null end else null end) as "PreviousAppointmentDate"
+then date(pat.start_date_time) else null end else null end) as "Date Of Previous Appointment"
 FROM patient_appointment
 join patient_appointment pat on pat.patient_id=patient_appointment.patient_id
 JOIN
@@ -25,5 +25,5 @@ WHERE DATE(patient_appointment.start_date_time) BETWEEN DATE('#startDate#') AND 
 AND patient_appointment.status='Scheduled'
 AND person_attribute.person_attribute_type_id in (select person_attribute_type_id from person_attribute_type where name ='Patient phone number 1')
 AND appointment_service.appointment_service_id=patient_appointment.appointment_service_id
-group by ID
+group by patient_identifier.identifier
 
