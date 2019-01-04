3d318638d65d4e9ecc1b2e0512e38d9dad774494
vishnu <dvishnu@thoughtworks.com>
2019-01-04 15:39:18 +0530

    Vishnu|Adding the multiple appointments visibility to scheduled report

--- openmrs/apps/reports/sql/scheduledAppointments.sql
+++ openmrs/apps/reports/sql/scheduledAppointments.sql
@@ -1,8 +1,8 @@
-SELECT 
-patient_identifier.identifier AS "Patient ID",
+SELECT
+ patient_identifier.identifier AS "Patient ID",
 concat(person_name.given_name,' ',person_name.family_name) AS "Patient Name",
 person_attribute.value AS "Phone Number",appointment_service.name AS "Service Type",
-DATE(patient_appointment.start_date_time) AS "Date Of Scheduled Appointment",
+Date(patient_appointment.start_date_time) AS "Date Of Scheduled Appointment",
 (case when pat.patient_appointment_id < patient_appointment.patient_appointment_id  then case when pat.patient_appointment_id < patient_appointment.patient_appointment_id
 then pat.status else null end else null end) as "Previous Appointment Status",
 (case when pat.patient_appointment_id < patient_appointment.patient_appointment_id  then case when pat.patient_appointment_id < patient_appointment.patient_appointment_id
@@ -21,9 +21,8 @@ appointment_service.appointment_service_id=patient_appointment.appointment_servi
 JOIN
 person_attribute ON
 person_attribute.person_id=patient_appointment.patient_id
+AND patient_appointment.status != 'Missed'
 WHERE DATE(patient_appointment.start_date_time) BETWEEN DATE('#startDate#') AND DATE('#endDate#')
-AND patient_appointment.status='Scheduled'
 AND person_attribute.person_attribute_type_id in (select person_attribute_type_id from person_attribute_type where name ='Patient phone number 1')
 AND appointment_service.appointment_service_id=patient_appointment.appointment_service_id
-group by patient_identifier.identifier
-
+group by patient_appointment.patient_appointment_id
